324e012f8aa4c318279c9ec6fa763d9285598e87
noosabaktee <ramanusabakti03@gmail.com>
2026-06-30 14:57:31 +0700

    update sharepoint link

--- resources/views/dashboard/reports.blade.php
+++ resources/views/dashboard/reports.blade.php
@@ -5,7 +5,7 @@
 ])
 
 @php
-    $sharePointReportUrl = 'https://useful-partner-98b.notion.site/ebd//38a7861b550e80cf8fb3f509fdc28ec8?v=38a7861b550e80a98cf8000c308ef3dc';
+    $sharePointReportUrl = 'https://docs.google.com/spreadsheets/d/1T0C8mc1QFMEwuZJaQlM8iEPN45vZPa1Kp52X56nPtCA/edit?usp=sharing';
 @endphp
 
 @section('content')
@@ -14,7 +14,7 @@
             <h2>Report</h2>
             <p>Embedded SharePoint folder for internship report files.</p>
         </div>
-        <a class="btn btn-primary btn-add" href="{{ $sharePointReportUrl }}" target="_blank" rel="noopener" style="text-decoration:none;"><i class="fa-solid fa-arrow-up-right-from-square"></i> Open Notion</a>
+        <a class="btn btn-primary btn-add" href="https://onekn-my.sharepoint.com/:f:/g/personal/irpan_pamil_kalbenutritionals_com/Ep6fkRRjaqhCq6xy5BnPq8UBhJCG1hsLrAbpi5LhRUa0_w?e=wMiusc" target="_blank" rel="noopener" style="text-decoration:none;"><i class="fa-solid fa-arrow-up-right-from-square"></i> Open Sharepoint</a>
     </div>
 
     <div class="card report-embed-card">
