b9fea4fe0274c6fc7eb64d7249f443c5ea166a8f
noosabaktee <ramanusabakti03@gmail.com>
2026-07-01 14:59:18 +0700

    update spreadsheet button

--- resources/views/dashboard/reports.blade.php
+++ resources/views/dashboard/reports.blade.php
@@ -14,7 +14,10 @@
             <h2>Report</h2>
             <p>Embedded SharePoint folder for internship report files.</p>
         </div>
-        <a class="btn btn-primary btn-add" href="https://onekn-my.sharepoint.com/:f:/g/personal/irpan_pamil_kalbenutritionals_com/Ep6fkRRjaqhCq6xy5BnPq8UBhJCG1hsLrAbpi5LhRUa0_w?e=wMiusc" target="_blank" rel="noopener" style="text-decoration:none;"><i class="fa-solid fa-arrow-up-right-from-square"></i> Open Sharepoint</a>
+        <div class="d-flex gap-2 flex-wrap">
+            <a class="btn btn-primary btn-add" href="https://docs.google.com/spreadsheets/d/1T0C8mc1QFMEwuZJaQlM8iEPN45vZPa1Kp52X56nPtCA/edit?usp=sharing" target="_blank" rel="noopener" style="text-decoration:none;"><i class="fa-solid fa-arrow-up-right-from-square"></i> Open Spreadsheet</a>
+            <a class="btn btn-primary btn-add" href="https://onekn-my.sharepoint.com/:f:/g/personal/irpan_pamil_kalbenutritionals_com/Ep6fkRRjaqhCq6xy5BnPq8UBhJCG1hsLrAbpi5LhRUa0_w?e=wMiusc" target="_blank" rel="noopener" style="text-decoration:none;"><i class="fa-solid fa-arrow-up-right-from-square"></i> Open Sharepoint</a>
+        </div>
     </div>
 
     <div class="card report-embed-card">
